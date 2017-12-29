df5dedcd8e8c5a496ae865a5c0eb8f681fe01db7
cyrusn <cyrusncy@gmail.com>
2017-12-30 02:27:44 +0800

    rename function and package

--- main.go
+++ main.go
@@ -1,24 +1,22 @@
-// Package goTestHelper stores some helper func for testing package
-package goTestHelper
+// Package assert stores some helper func for testing package
+package assert
 
 import (
 	"reflect"
 	"testing"
 )
 
-// OK fails the test if err is not nil
-func OK(t *testing.T, err error) {
-	if err != nil {
-		t.Fatal(err)
+// OK fails the test if v is not nil
+func OK(t *testing.T, v interface{}) {
+	if v != nil {
+		t.Fatal(v)
 	}
 }
 
-// ExpectError fails the test if the f didn't fail
-func ExpectError(name string, t *testing.T, f func()) {
+// Panic fails the test if the f didn't fail
+func Panic(name string, t *testing.T, f func()) {
 	defer func(t *testing.T) {
-		err := recover()
-
-		if err == nil {
+		if err := recover(); err == nil {
 			t.Fatalf("Error Test: [%s] did not return error", name)
 		}
 	}(t)
@@ -28,7 +26,6 @@ func ExpectError(name string, t *testing.T, f func()) {
 // Equal fails the test if got is not equal to want
 func Equal(got, want interface{}, t *testing.T) {
 	if !reflect.DeepEqual(want, got) {
-
 		t.Errorf(
 			"Incorrect!\ngot: %v\nwant: %v.\n",
 			got,
